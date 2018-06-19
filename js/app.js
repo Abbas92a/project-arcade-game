2ea30a6d702086418119c81dc853b373e2cbac96
Mohamed Abbas <mohamedabbas92a@gmail.com>
2018-06-19 04:41:27 +0200

    Feat: Add movement to enemies

--- js/app.js
+++ js/app.js
@@ -1,8 +1,32 @@
+let count = 1;
+
 // Enemies our player must avoid
-var Enemy = function() {
+function Enemy(y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x= -100,
+    this.y= y,
+    this.update = (function(dt) {
+        // You should multiply any movement by the dt parameter
+        // which will ensure the game runs at the same speed for
+        // all computers.
 
+        let speedFactor =1;
+
+        return function (dt) {
+          if (this.x > 1000) {
+            this.x = -100;
+          }
+
+          if (this.x=== -100) {
+            speedFactor = Math.random();
+          }
+          // console.log (this.x);
+          // console.log (this.x + speedFactor * 400 * dt);
+          // debugger;
+          this.x += (speedFactor * 400 * dt);
+        };
+    })();
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -10,11 +34,28 @@ var Enemy = function() {
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+
+// Enemy.prototype.update = (function(dt) {
+//     // You should multiply any movement by the dt parameter
+//     // which will ensure the game runs at the same speed for
+//     // all computers.
+//
+//     let speedFactor =1;
+//
+//     return function (dt) {
+//       if (this.x > 500) {
+//         this.x = -100;
+//       }
+//
+//       if (this.x=== -100) {
+//         speedFactor = Math.random();
+//       }
+//       // console.log (this.x);
+//       // console.log (this.x + speedFactor * 400 * dt);
+//       // debugger;
+//       this.x += (speedFactor * 400 * dt);
+//     };
+// })();
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -24,13 +65,32 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
+function Player(){
+  this.x= 300,
+  this.y= 50,
+  this.sprite = 'images/char-boy.png';
+  this.update = function() {
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  };
+  this.render = function() {
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  };
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+let enemy1 = new Enemy(60);
+let enemy2 = new Enemy(60);
+let enemy3 = new Enemy(145);
+let enemy4 = new Enemy(145);
+let enemy5 = new Enemy(230);
+let enemy6 = new Enemy(230);
+// let allEnemies = [enemy1];
+let allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6];
 
+let player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
